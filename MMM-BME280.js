46be8de0fd9699bd7b06e57dcac81ea2a32dfaba
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-04-11 23:11:23 +0200

    fix sizing

--- MMM-BME280.js
+++ MMM-BME280.js
@@ -1,13 +1,14 @@
 
 Module.register("MMM-BME280", {
-    // Default module config.
-    defaults: {
-        updateInterval: 100, // Seconds
-        titleText: "Home weather",
-        deviceAddress: "0x76",
-        temperatureScaleType: 0, // Celsuis
-        pressureScaleType: 0 // hPa
-    },
+  // Default module config.
+  defaults: {
+    updateInterval: 100, // Seconds
+    titleText: "Home weather",
+    deviceAddress: "0x76",
+    temperatureScaleType: 0, // Celsuis
+    pressureScaleType: 0, // hPa
+    size: "small" // You can use any official classes: https://forum.magicmirror.builders/topic/346/resize-custom-or-main-modules
+  },
 
     // Define start sequence.
     start: function () {
@@ -35,14 +36,9 @@ Module.register("MMM-BME280", {
     getDom: function () {
         var wrapper = document.createElement("div");
 
-        var header = document.createElement("div");
-        var label = document.createTextNode(this.config.titleText);
-        header.className = 'bme-header';
-        header.appendChild(label)
-        wrapper.appendChild(header);
-
         var table = document.createElement("table");
-        var tbdy = document.createElement('tbody');
+        table.className = this.config.size;
+        var tbody = document.createElement("tbody");
         for (var i = 0; i < 3; i++) {
             var val = "";
             var sufix = "";
@@ -85,25 +81,23 @@ Module.register("MMM-BME280", {
             var tr = document.createElement('tr');
             var icon = document.createElement("i");
 
-            icon.className = 'fa fa-' + icon_img + ' bme-icon';
+            icon.className = `fa fa-${icon_img} bme-icon  ${this.config.size}`;
 
-            var text_div = document.createElement("div");
             var text = document.createTextNode(" " + val + sufix);
-            text_div.className = 'bme-text';
-            text_div.appendChild(text);
 
             var td = document.createElement('td');
             td.className = 'bme-td-icon';
             td.appendChild(icon)
             tr.appendChild(td)
 
-            var td = document.createElement('td');
-            td.appendChild(text_div)
-            tr.appendChild(td)
+            var texttd = document.createElement("td");
+            texttd.className = `bme-text ${this.config.size}`;
+            texttd.appendChild(text);
+            tr.appendChild(texttd);
 
-            tbdy.appendChild(tr);
+            tbody.appendChild(tr);
         }
-        table.appendChild(tbdy);
+        table.appendChild(tbody);
         wrapper.appendChild(table);
 
         return wrapper;
